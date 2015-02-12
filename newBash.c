ef042bea6edcfb588ccf9aba161c8cf37742ea96
Adam7700 <adampog77@gmail.com>
2015-02-12 13:55:30 -0500

    Updated code

--- newBash.c
+++ newBash.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <linux/limits.h>
+#include <sys/types.h>
+#include <string.h>
+
+#define arrLen(a) (sizeof(a) / sizeof(*a))
+
+void parseString(char*, char*[]);
 
 int main(){
     char input[BUFSIZ];
@@ -12,13 +19,64 @@ int main(){
         if (strcmp(input,"exit")==0){
             return 0;
         }
-        char *argv[]={"/etc\0"};
-        execv("/bin/ls",argv);
-        printf(">>");
+        pid_t child;
+            
+        if((child = fork()) == 0){
+            char* builtIn[] = {"cd", "pwd", "version"};
+            int isBuiltIn = 0;
+
+            int i;
+            for(i=0; i<arrLen(builtIn); i++){
+                if (strcmp(input, builtIn[i])==0){
+                    isBuiltIn = 1;
+                }
+            }
+            
+
+            
+            if(isBuiltIn){
+                if(strcmp(input, "version")==0){
+                    printf("%s\n","Version: 0.01");
+                }
+                else if(strcmp(input, "pwd")==0){
+                   char cwd[PATH_MAX];
+                   char* curPath = getcwd(cwd, sizeof(cwd));
+                   printf("%s\n", curPath);
+                }
+                else if(strcmp(input, "cd")==0){
+                    char inputString[1024] = "cd /there";
+                    char* arr[10];
+                    parseString(inputString,arr );
+                }
+            }
+            else{
+                char *argv[]={input};
+                execv(input,argv);
+
+
+            }
+            return 0;
+        }
+        else{
+            waitpid(child);
+            printf(">>");
+        }
+
     }
 }
 
+void parseString(char* inputString, char*arr[]){
+        printf("%s\n", inputString);
+        char *delim = " ";
+    char *currentTok  = strtok (inputString , delim);
 
 
-//njwhite777@gmail.com 
-//njwhite777
+    while(currentTok!= NULL){
+        printf("%s\n", currentTok);
+        currentTok = strtok(NULL, delim);
+    }
+
+    
+    return;
+} 
+
