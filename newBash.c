cce81f389ce76f3244be697e724ca6cdbbc57281
nwhite <nwhite@cse.taylor.edu>
2015-02-13 09:14:33 -0500

    Checking path mostly working! Need to handle any 'edge cases' that might come up.

--- newBash.c
+++ newBash.c
@@ -7,7 +7,8 @@
 
 #define arrLen(a) (sizeof(a) / sizeof(*a))
 
-int parseString(char*, char*[]);
+int parseString(char*, char*[],char*);
+char strLastChr(char *);
 
 int main(){
    
@@ -26,7 +27,7 @@ int main(){
         if(input[len-1] == '\n'){
             input[len-1] = 0;
         }
-        paramLen = parseString(input,arr);
+        paramLen = parseString(input,arr," \t");
 
         if(paramLen != -1){
             if (strcmp(arr[0],"exit")==0){
@@ -72,13 +73,26 @@ int main(){
                     // run it.
                     
                     printf("Child is now, %d\n",child);
-
                     execv(arr[0],arr);
                 }
                 else if((access(arr[0],F_OK)!=0) && ((child = fork())== 0)){
                     //The file does not exist, check for it in the path
                     //
-                    execv(arr[0],arr);
+                    char *path_arr[20] = {0};
+                    int path_count = parseString(getenv("PATH"),path_arr,":");
+                    int m;
+                    for(m=0;m<path_count;m++){
+                        char full_path[256];
+                        strcpy(full_path,path_arr[m]);
+                        if(strLastChr(path_arr[m]) != '/'){
+                            strcat(full_path,"/");
+                        }
+                        strcat(full_path,arr[0]);
+                        if(access(full_path,F_OK)==0){
+                            arr[0] = full_path;
+                            execv(full_path,arr);
+                        }
+                    }
                 }
                 if(child > 0){
                     waitpid(child);
@@ -91,9 +105,12 @@ int main(){
     }
 }
 
-int parseString(char* inputString, char *arr[]){
+char strLastChr(char  *string){
+    return string[strlen(string)-1];
+}
+
+int parseString(char* inputString, char *arr[],char *delim){
     if(strlen(inputString) != 0){
-        char *delim = " ";
         char *currentTok  = strtok (inputString , delim);
         int count=0;
         while(currentTok){
